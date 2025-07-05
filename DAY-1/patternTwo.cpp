1040ae36070e37894c033f39cbd9a7d817f2f5a8
kanhaiya1202 <kanhaiyasharma1202@gamil.com>
2025-07-05 22:21:38 +0530

    patternCode

--- DAY-1/patternTwo.cpp
+++ DAY-1/patternTwo.cpp
@@ -1,2 +1,30 @@
+// show pattern 
+
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+
 #include<iostream>
 using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the Number of value"<<endl;
+    cin>>n;
+
+    int i = 0;
+    while (i < n)
+    {
+        i++;
+        int j = 0;
+        while (j < n)
+        {  
+            j++;
+            cout<<" "<<j<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
